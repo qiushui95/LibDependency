1cdbba6e6999b214343223769f83d72e51d1d39b
YCX-Company <974577817@qq.com>
2021-12-17 17:53:07 +0800

    update

--- lib/src/test/kotlin/son/ysy/dependencies/DependencyConfig.kt
+++ lib/src/test/kotlin/son/ysy/dependencies/DependencyConfig.kt
@@ -191,7 +191,7 @@ sealed class DependencyConfig(
         object Startup : DependencyConfig(
             group = "com.github.qiushui95",
             name = "AndroidInitializer",
-            version = "1.0.5",
+            version = "1.0.6",
             remark = "app启动初始化",
             link = "https://github.com/qiushui95/AndroidInitializer",
             dependencyMethod = DependencyMethod.Implementation,
